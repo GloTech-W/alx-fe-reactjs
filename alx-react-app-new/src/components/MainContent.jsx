5bea5318474aa4c7af53603995e5bee0a174b13b
Glory Gakingo <gakingoglory02@gmail.com>
2024-08-15 22:32:08 +0300

    Add new React app directory

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+// src/components/MainContent.jsx
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
