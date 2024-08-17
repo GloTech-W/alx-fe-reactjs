f412c20c7ee40cb67d9eca9e91cd7898382dcc69
Glory Gakingo <gakingoglory02@gmail.com>
2024-08-17 14:41:30 +0300

    Add Counter component and integrate it into App component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,6 +5,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter'; // Import the Counter component
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -46,6 +47,9 @@ const App = () => {
 
       {/* Add the UserProfile component here */}
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      {/* Add the Counter component here */}
+      <Counter />
     </>
   );
 };
