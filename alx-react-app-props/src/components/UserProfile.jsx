a63cff0e04144cdb68d3663230f09f2637413def
Glory Gakingo <gakingoglory02@gmail.com>
2024-08-18 21:21:43 +0300

    modifications to UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,16 @@
-// src/components/UserProfile.jsx
 import React from 'react';
+import { useUser } from './UserContext';
 
-const UserProfile = (props) => {
+const UserProfile = ({ name, age, bio }) => {
+  const userData = useUser(); // Use the custom hook to access the context
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+      <p>Age: {age}</p>
+      <p>Bio: {bio}</p>
     </div>
   );
 };
