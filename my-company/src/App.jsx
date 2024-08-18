97c0c9c05cb046e090af73658bac5d8339546cd3
Glory Gakingo <gakingoglory02@gmail.com>
2024-08-18 23:20:47 +0300

    Move Navbar component to components directory and update App.jsx import

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,19 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
-
-function Navbar() {
-  return (
-    <nav style={{ padding: '10px', background: '#eee' }}>
-      <Link to="/" style={{ margin: '10px' }}>Home</Link>
-      <Link to="/about" style={{ margin: '10px' }}>About</Link>
-      <Link to="/services" style={{ margin: '10px' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '10px' }}>Contact</Link>
-    </nav>
-  );
-}
+import Navbar from './components/Navbar'; // Import Navbar from components directory
 
 function App() {
   return (
