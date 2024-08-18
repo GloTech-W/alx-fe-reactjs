83e7b78626765fbce0ba7918f828244d08204a84
Glory Gakingo <gakingoglory02@gmail.com>
2024-08-18 23:36:23 +0300

    Restore and organize files in components directory

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <nav style={{ backgroundColor: '#eee', padding: '10px', display: 'flex', justifyContent: 'center' }}>
+      <ul style={{ listStyleType: 'none', margin: 0, padding: 0, display: 'flex' }}>
+        <li style={{ margin: '0 15px' }}><Link to="/">Home</Link></li>
+        <li style={{ margin: '0 15px' }}><Link to="/about">About</Link></li>
+        <li style={{ margin: '0 15px' }}><Link to="/services">Services</Link></li>
+        <li style={{ margin: '0 15px' }}><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
