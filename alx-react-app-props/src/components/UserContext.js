5298203d6bd1619adaaba42d53d0e2d4820fac68
Glory Gakingo <gakingoglory02@gmail.com>
2024-08-18 21:14:31 +0300

    modifications to UserContext.js

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,10 +1,9 @@
-// src/UserContext.js
 import React, { createContext, useContext } from 'react';
 
 const UserContext = createContext();
 
-export const UserProvider = ({ children, value }) => {
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+export const UserProvider = ({ children }) => {
+  return <UserContext.Provider>{children}</UserContext.Provider>;
 };
 
 export const useUser = () => useContext(UserContext);
