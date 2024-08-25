e69945c80563539ed9186953af428b01ce8ad6ab
Glory Gakingo <gakingoglory02@gmail.com>
2024-08-25 22:07:05 +0300

    Update Zustand store to include search functionality

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,17 +3,37 @@ import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+
+  // Function to set the search term
+  setSearchTerm: (term) => {
+    set({ searchTerm: term });
+    set(state => ({
+      filteredRecipes: state.recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      )
+    }));
+  },
+
+  // Function to add a new recipe
   addRecipe: (newRecipe) => set((state) => ({
     recipes: [...state.recipes, newRecipe]
   })),
+
+  // Function to delete a recipe by ID
   deleteRecipe: (recipeId) => set((state) => ({
     recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
   })),
+
+  // Function to update an existing recipe
   updateRecipe: (updatedRecipe) => set((state) => ({
     recipes: state.recipes.map(recipe =>
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     )
   })),
+
+  // Function to set the initial recipes list
   setRecipes: (recipes) => set({ recipes })
 }));
 
