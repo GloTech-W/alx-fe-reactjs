4ee3c09d70e94ed956ff37fcb7789f8db300bff4
Glory Gakingo <gakingoglory02@gmail.com>
2024-09-01 15:49:01 +0300

    Update Profile component with nested routes and modify ProfileDetails component

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,17 @@
+// src/pages/ProfileDetails.jsx
+import React from 'react';
+
+function ProfileDetails() {
+  return <div>Profile Details</div>;
+}
+
+export default ProfileDetails;
+
+// src/pages/ProfileSettings.jsx
+import React from 'react';
+
+function ProfileSettings() {
+  return <div>Profile Settings</div>;
+}
+
+export default ProfileSettings;
