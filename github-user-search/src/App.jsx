8477cbccb3a855a18a319d1145aaa9516c429f4d
Glory Gakingo <gakingoglory02@gmail.com>
2024-09-22 16:55:10 +0300

    Implement search functionality in GitHub User Search App: add Search component and API integration

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Search from "./components/Search"; // Import the Search component
 
-const Home = () => <div>Welcome to GitHub User Search</div>;
+const Home = () => (
+  <div>
+    <h1>Welcome to GitHub User Search</h1>
+    <Search /> {/* Add the Search component here */}
+  </div>
+);
 
 function App() {
   return (
